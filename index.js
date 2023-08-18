dcea021c0c8829d819b4ce7ca47f7104d6f0a555
Naoto Yamada <naoto.yamada@sevenrich.jp>
2023-08-18 20:32:46 +0900

    不具合再現

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
+const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+const sqs = new SQSClient();
+
+const producer = async (event) => {
+  let statusCode = 200;
+  let message;
+
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "No body was found",
+      }),
+    };
+  }
+
+  try {
+    await sqs.send(new SendMessageCommand({
+      QueueUrl: process.env.QUEUE_URL,
+      MessageBody: event.body,
+      MessageAttributes: {
+        AttributeName: {
+          StringValue: "Attribute Value",
+          DataType: "String",
+        },
       },
-      null,
-      2
-    ),
+    }));
+
+    message = "Message accepted!";
+  } catch (error) {
+    console.log(error);
+    message = error;
+    statusCode = 500;
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify({
+      message,
+    }),
   };
+};
+
+const consumer = async (event) => {
+  for (const record of event.Records) {
+    const messageAttributes = record.messageAttributes;
+    console.log(
+      "Message Attribute: ",
+      messageAttributes.AttributeName.stringValue
+    );
+    console.log("Message Body: ", record.body);
+
+  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("end");
+      resolve();
+    }, 3000);
+  })
+    
+};
+
+module.exports = {
+  producer,
+  consumer,
 };
